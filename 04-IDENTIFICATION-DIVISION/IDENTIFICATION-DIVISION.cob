b57fda97ee989a24056cb6db5074b410356f7cc1
Albert Manjón <albertmanjon123@gmail.com>
2024-12-03 20:10:12 +0100

    IDENTIFICATION-DIVISION

---
+++ 04-IDENTIFICATION-DIVISION/IDENTIFICATION-DIVISION.cob
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.         AS20.
+       AUTHOR.            ALBERT.
+       INSTALLATION.      LINUX. 
+       DATE-WRITTEN.      02/12/2024.
+       SECURITY.          CONFIDENTIAL.
